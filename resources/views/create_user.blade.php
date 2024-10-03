a071cedc6c31c1fb7be14cbc862bf820596c77b7
Rizki Mahesa <kikimahesa2020@gmail.com>
2024-10-04 01:57:57 +0700

    perubahan 4 tampilan daftar user

--- resources/views/create_user.blade.php
+++ resources/views/create_user.blade.php
@@ -1,33 +1,32 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+@extends('layouts.app')
+
+@section('content')
+<div class="form-container">
+    <h2>Isi Data User</h2>
+    <form action="{{ route('user.store') }}" method="POST">
     <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
-</head>
-<body>
-    <form action="{{ route('user.store') }}" method="POST"> 
         @csrf
-        <label for = "nama" >Nama: </label>
-        <input type = "text" id = "nama" name = "nama">
-                @foreach($errors->get('nama') as $msg)
-                <p class="text-danger">{{ $msg }}</p>
-                @endforeach
+        <div class="form-group">
+            <label for="nama">Nama:</label>
+            <input type="text" id="nama" name="nama" class="form-control" placeholder="Masukkan Nama" required>
+        </div>
 
-        <label for = "npm" >NPM: </label>
-        <input type = "text" id = "npm" name = "npm">
-                @foreach($errors->get('npm') as $msg)
-                <p class="text-danger">{{ $msg }}</p>
-                @endforeach
+        <div class="form-group">
+            <label for="npm">NPM:</label>
+            <input type="text" id="npm" name="npm" class="form-control" placeholder="Masukkan NPM" required>
+        </div>
 
-        <label for = "id_kelas" >Kelas: </label><br>
-        <select name="kelas_id" id="kelas_id" required>
-                @foreach($kelas as $kelasItem)
-                <option value = "{{$kelasItem->id}}">{{$kelasItem->nama_kelas}}</option>
+        <div class="form-group">
+            <label for="kelas">Kelas:</label>
+            <select name="kelas_id" id="kelas_id" class="form-control" required>
+                <option value="" disabled selected>Pilih Kelas</option>
+                @foreach ($kelas as $kelasItem)
+                    <option value="{{ $kelasItem->id }}">{{ $kelasItem->nama_kelas }}</option>
                 @endforeach
-        </select>
-        <button type = "submit" >Submit </button>
+            </select>
+        </div>
+
+        <button type="submit" class="btn btn-primary">Submit</button>
     </form>
-</body>
-</html>
+</div>
+@endsection
